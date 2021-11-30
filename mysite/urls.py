006fba4e8a9f7ea0cbb64ed6d38e402f96703099
Lee Shiueh <lee.shiueh@gmail.com>
2021-11-30 13:30:41 +0800

    Add changes for gae flex env trial

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,11 +13,24 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import os
 from django.conf.urls import include
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
+from django.views.generic import View
+from django.http import JsonResponse
+
+
+class EnvView(View):
+
+    def get(self, request):
+        return JsonResponse({
+            'REDIS_HOST': os.getenv('REDIS_HOST')
+        })
+
 
 urlpatterns = [
     path('chat/', include('chat.urls')),
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path(r'^$', EnvView.as_view()),
 ]
